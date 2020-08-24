7e6bda9b31f01ce8237effe932b198e1290f7ab7
Alex <alexroth96@gmail.com>
2020-08-24 17:51:44 -0400

    fix design tweaks

--- src/script.js
+++ src/script.js
@@ -12,12 +12,10 @@
     if (windowDiff > ad.getBoundingClientRect().height) {
       adCont.style.position = "relative";
       adCont.style.top = "82%";
-      console.log(windowDiff, ad.getBoundingClientRect().height);
     }
     if (windowDiff < ad.getBoundingClientRect().height) {
       adCont.style.position = "fixed";
       adCont.style.top = "15%";
-      console.log("fixed", windowDiff, ad.getBoundingClientRect().height);
     }
   };
 
