a04bcf88769cf4a9576790fd93ab9b2748a6c4b9
Alex <alexroth96@gmail.com>
2020-08-24 16:05:51 -0400

    add nav and ad scrolling position module

---
+++ src/script.js
@@ -0,0 +1,25 @@
+//module to handle positioning on scroll
+(() => {
+  const selectors = {
+    adRight: document.querySelector(".ad-right"),
+    adContainer: document.querySelector(".ad-container"),
+  };
+  const handleScrolling = () => {
+    const ad = selectors.adRight;
+    const adCont = selectors.adContainer;
+    const windowDiff =
+      window.pageYOffset + adCont.getBoundingClientRect().height;
+    if (windowDiff > ad.getBoundingClientRect().height) {
+      adCont.style.position = "relative";
+      adCont.style.top = "82%";
+      console.log(windowDiff, ad.getBoundingClientRect().height);
+    }
+    if (windowDiff < ad.getBoundingClientRect().height) {
+      adCont.style.position = "fixed";
+      adCont.style.top = "15%";
+      console.log("fixed", windowDiff, ad.getBoundingClientRect().height);
+    }
+  };
+
+  window.addEventListener("scroll", handleScrolling);
+})();
